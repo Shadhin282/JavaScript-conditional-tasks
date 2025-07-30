8f68be604a0aa002cc7241bf4847ae66227d6c8a
Md. Mahmodul Hassen Khan Shadin <shadhinkhan28@gmail.com>
2025-07-30 15:35:34 +0000

    add commitment.js

---
+++ commitment.js
@@ -0,0 +1,14 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+let sms = "I will invest at least 6 hrs every single day for next 60 days!"
+let i =0;
+while(i<=60){
+    console.log(sms);
+    i++
+};
